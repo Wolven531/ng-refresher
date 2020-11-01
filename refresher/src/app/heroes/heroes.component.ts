2e802b5b8cc587641db76afd8bf2b652fc9a86f4
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-01 15:48:09 -0700

    Add heroes component

---
+++ refresher/src/app/heroes/heroes.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-heroes',
+	styleUrls: ['./heroes.component.sass'],
+	templateUrl: './heroes.component.html',
+})
+export class HeroesComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+}

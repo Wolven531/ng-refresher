b91e7951309bc10f0283515550cee32e6fc0e449
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-02 22:38:46 -0700

    Make custom pipe available to app - capitalize

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
+import { CapitalizePipe } from './capitalize.pipe'
 import { HeroesComponent } from './heroes/heroes.component'
 
 @NgModule({
@@ -8,6 +9,7 @@ import { HeroesComponent } from './heroes/heroes.component'
 	declarations: [
 		AppComponent,
 		HeroesComponent,
+		CapitalizePipe,
 	],
 	imports: [
 		BrowserModule,

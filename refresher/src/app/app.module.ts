51bcd3ce3f4042467e8e6438e218e5bfc9f2fdcc
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-06 20:33:58 -0700

    Add initial HeroDetail comp

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { CapitalizePipe } from './capitalize.pipe'
-import { HeroesComponent } from './heroes/heroes.component'
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 
 @NgModule({
 	bootstrap: [AppComponent],
@@ -11,6 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component'
 		AppComponent,
 		HeroesComponent,
 		CapitalizePipe,
+		HeroDetailComponent,
 	],
 	imports: [
 		BrowserModule,

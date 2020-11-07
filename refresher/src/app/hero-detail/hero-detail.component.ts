5a5250bf9a8d85021539a459dfb25e546d53faa1
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-06 20:47:56 -0700

    Add Input prop for HeroDetail

--- refresher/src/app/hero-detail/hero-detail.component.ts
+++ refresher/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+
+import { Hero } from '../hero.interface'
 
 @Component({
 	selector: 'app-hero-detail',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core'
 	templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent implements OnInit {
+	@Input() hero: Hero
+
 	constructor() { }
 
 	ngOnInit(): void {

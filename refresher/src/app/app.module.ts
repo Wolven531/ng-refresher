f84a1f38b5073081f12d3658fc6f8a7dbe25fa12
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-07 18:15:58 -0700

    Add boilerplate for messages comp

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { CapitalizePipe } from './capitalize.pipe'
-import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'
+import { HeroesComponent } from './heroes/heroes.component'
+import { MessagesComponent } from './messages/messages.component'
 
 @NgModule({
 	bootstrap: [AppComponent],
@@ -13,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 		HeroesComponent,
 		CapitalizePipe,
 		HeroDetailComponent,
+		MessagesComponent,
 	],
 	imports: [
 		BrowserModule,
---
+++ refresher/src/app/messages/messages.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+	selector: 'app-messages',
+	styleUrls: ['./messages.component.sass'],
+	templateUrl: './messages.component.html',
+})
+export class MessagesComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+}

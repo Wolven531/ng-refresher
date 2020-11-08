35c9f6cde7e3e56e6255361561a63d94bd99b4af
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-07 17:44:06 -0700

    Implement HeroService w/ mock data

--- refresher/src/app/hero.service.ts
+++ refresher/src/app/hero.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core'
+import { Hero } from '../app/hero.interface'
+import { HEROES } from '../mock-heroes'
 
 @Injectable({
 	providedIn: 'root',
 })
 export class HeroService {
 	constructor() { }
+
+	getHeroes(): Hero[] {
+		return HEROES
+	}
 }

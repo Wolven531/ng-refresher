2d238c8f199dffad4626aa1e4bd3f43ae364cb11
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-07 18:23:54 -0700

    Use MessageService in HeroService

--- refresher/src/app/hero.service.ts
+++ refresher/src/app/hero.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core'
 import { Observable, of } from 'rxjs'
 import { Hero } from '../app/hero.interface'
 import { HEROES } from '../mock-heroes'
+import { MessageService } from './message.service'
 
 @Injectable({
 	providedIn: 'root',
 })
 export class HeroService {
-	constructor() { }
+	private static MSG_HEROES_FETCHED = '[ HeroService ] heroes fetched'
+
+	constructor(
+		private readonly messageService: MessageService,
+	) { }
 
 	getHeroes(): Observable<Hero[]> {
+		this.messageService.add(HeroService.MSG_HEROES_FETCHED)
+
 		return of(HEROES)
 	}
 }

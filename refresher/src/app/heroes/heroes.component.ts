b88c4c8d0b36c7763c1bf37bbef332e614884059
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-07 18:38:26 -0700

    Add message when hero is selected

--- refresher/src/app/heroes/heroes.component.ts
+++ refresher/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from '../hero.interface'
 import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service'
 
 @Component({
 	selector: 'app-heroes',
@@ -8,11 +9,14 @@ import { HeroService } from '../hero.service'
 	templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
+	private static MSG_HERO_SELECTED = '[ HeroComp ] hero selected'
+
 	heroes: Hero[]
 	selectedHero: Hero
 
 	constructor(
 		private readonly heroService: HeroService,
+		private readonly messageService: MessageService,
 	) { }
 
 	loadHeroes(): void {
@@ -26,5 +30,7 @@ export class HeroesComponent implements OnInit {
 
 	onHeroSelect(hero: Hero): void {
 		this.selectedHero = hero
+
+		this.messageService.add(`${HeroesComponent.MSG_HERO_SELECTED}, id=${hero.id}`)
 	}
 }

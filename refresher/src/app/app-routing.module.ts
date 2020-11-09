865db710e7d28715979b74ce23c13df679f72394
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-08 22:11:31 -0700

    Add Heroes route

--- refresher/src/app/app-routing.module.ts
+++ refresher/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HeroesComponent } from './heroes/heroes.component'
+
+const routes: Routes = [
+	{
+		component: HeroesComponent,
+		path: 'heroes',
+	},
+]
 
 @NgModule({
 	declarations: [],
+	exports: [RouterModule],
 	imports: [
-		CommonModule,
+		RouterModule.forRoot(routes),
 	],
 })
 export class AppRoutingModule { }

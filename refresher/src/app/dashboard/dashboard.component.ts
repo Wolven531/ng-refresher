7cd3e03fa0fdca565321dfdca46f6a73187f7255
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-09 19:02:03 -0700

    Add loadHeroes() to dashboard comp

--- refresher/src/app/dashboard/dashboard.component.ts
+++ refresher/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { Hero } from '../hero.interface'
+import { HeroService } from '../hero.service'
 
 @Component({
 	selector: 'app-dashboard',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core'
 	templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
-	constructor() { }
+	heroes: Hero[]
+
+	constructor(
+		private readonly heroService: HeroService,
+	) {
+		this.heroes = []
+	}
+
+	loadHeroes(): void {
+		this.heroService.getHeroes()
+			.subscribe(heroes => this.heroes = heroes.slice(1, 5))
+	}
 
 	ngOnInit(): void {
+		this.loadHeroes()
 	}
 }

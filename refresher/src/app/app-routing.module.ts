3561524994419c0817a4460515486837ec083b08
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-09 19:01:21 -0700

    Add path to dashboard and default path

--- refresher/src/app/app-routing.module.ts
+++ refresher/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { DashboardComponent } from './dashboard/dashboard.component'
 import { HeroesComponent } from './heroes/heroes.component'
 
 const routes: Routes = [
 	{
+		component: DashboardComponent,
+		path: 'dashboard',
+	},
+	{
 		component: HeroesComponent,
 		path: 'heroes',
+	},
+	{
+		path: '',
+		redirectTo: '/dashboard',
+		pathMatch: 'full',
 	},
 ]
 

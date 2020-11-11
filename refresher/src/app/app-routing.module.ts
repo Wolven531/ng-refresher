b2b5160aca30ab7fca3245ed5cb3a7e2e08d7e3f
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-10 22:37:09 -0700

    Add HeroDetail route

--- refresher/src/app/app-routing.module.ts
+++ refresher/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 import { HeroesComponent } from './heroes/heroes.component'
 
 const routes: Routes = [
 	{
 		component: DashboardComponent,
 		path: 'dashboard',
+	},
+	{
+		component: HeroDetailComponent,
+		path: 'detail/:id',
 	},
 	{
 		component: HeroesComponent,

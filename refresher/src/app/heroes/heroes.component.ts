db91e0c2eaffcd00aac1e28c26b3a460936ea932
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-10 22:38:09 -0700

    Remove unused code

--- refresher/src/app/heroes/heroes.component.ts
+++ refresher/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from '../hero.interface'
 import { HeroService } from '../hero.service'
-import { MessageService } from '../message.service'
 
 @Component({
 	selector: 'app-heroes',
@@ -9,14 +8,11 @@ import { MessageService } from '../message.service'
 	templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
-	private static MSG_HERO_SELECTED = '[ HeroComp ] hero selected'
-
 	heroes: Hero[]
 	selectedHero: Hero
 
 	constructor(
 		private readonly heroService: HeroService,
-		private readonly messageService: MessageService,
 	) { }
 
 	loadHeroes(): void {
@@ -26,11 +22,5 @@ export class HeroesComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.loadHeroes()
-	}
-
-	onHeroSelect(hero: Hero): void {
-		this.selectedHero = hero
-
-		this.messageService.add(`${HeroesComponent.MSG_HERO_SELECTED}, id=${hero.id}`)
 	}
 }

6d43c0a16c89e1203988783001a7b6bd8e98b789
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-12 20:34:59 -0700

    Add in mem db service

---
+++ refresher/src/app/in-memory-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core'
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api'
+import { Observable } from 'rxjs'
+import { HEROES } from '../mock-heroes'
+import { Hero } from './hero.interface'
+
+@Injectable({
+	providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+	constructor() { }
+
+	createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}> {
+		const heroes = HEROES
+
+		return { heroes }
+	}
+
+	// Overrides the genId method to ensure that a hero always has an id
+	//
+	// If heroes is empty, method returns initial number (11);
+	// otherwise, method returns highest hero id + 1
+	genId(heroes: Hero[]): number {
+		return heroes.length > 0
+			? Math.max(...heroes.map(hero => hero.id)) + 1
+			: 11
+	}
+}

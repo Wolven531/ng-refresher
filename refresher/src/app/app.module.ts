d1f631a4c9aee006a68a96d8090595d27f95727a
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-12 20:35:10 -0700

    Make in mem db available throughout app

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { CapitalizePipe } from './capitalize.pipe'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 import { HeroesComponent } from './heroes/heroes.component'
+import { InMemoryDataService } from './in-memory-data.service'
 import { MessagesComponent } from './messages/messages.component'
 
 @NgModule({
@@ -25,6 +27,14 @@ import { MessagesComponent } from './messages/messages.component'
 		FormsModule,
 		AppRoutingModule,
 		HttpClientModule,
+		// !!! TODO
+		// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+		// and returns simulated server responses;
+		// remove it when a server is ready to receive requests
+		HttpClientInMemoryWebApiModule.forRoot(
+			InMemoryDataService,
+			{ dataEncapsulation: false }
+		),
 	],
 	providers: [],
 })

95c67fc6df9b6bd1a925a8ce2a8ab28b532697e4
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-14 23:15:08 -0700

    Prep work for hero saving

--- refresher/src/app/hero-detail/hero-detail.component.ts
+++ refresher/src/app/hero-detail/hero-detail.component.ts
@@ -32,4 +32,9 @@ export class HeroDetailComponent implements OnInit {
 		this.heroService.getHero(id)
 			.subscribe(hero => this.hero = hero)
 	}
+
+	save(): void {
+		// this.heroService.updateHero(this.hero)
+		// 	.subscribe(() => this.goBack());
+	}
 }

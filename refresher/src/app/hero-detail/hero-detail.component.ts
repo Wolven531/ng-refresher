e3a4523e895c7f741af92bb69dacebd6808054ee
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-15 18:42:27 -0700

    Use hero service updateHero

--- refresher/src/app/hero-detail/hero-detail.component.ts
+++ refresher/src/app/hero-detail/hero-detail.component.ts
@@ -34,7 +34,7 @@ export class HeroDetailComponent implements OnInit {
 	}
 
 	save(): void {
-		// this.heroService.updateHero(this.hero)
-		// 	.subscribe(() => this.goBack());
+		this.heroService.updateHero(this.hero)
+			.subscribe(() => this.goBack())
 	}
 }

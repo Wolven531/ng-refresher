93d4617d087e2e6f14e47553c0f74dbdd67c8a75
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-15 18:50:25 -0700

    Add addHero to heroes component

--- refresher/src/app/heroes/heroes.component.ts
+++ refresher/src/app/heroes/heroes.component.ts
@@ -14,6 +14,19 @@ export class HeroesComponent implements OnInit {
 		private readonly heroService: HeroService,
 	) { }
 
+	addHero(name: string): void {
+		name = name.trim()
+
+		if (name.length < 1) {
+			return
+		}
+
+		this.heroService.addHero({ name } as Hero)
+			.subscribe(hero => {
+				this.heroes.push(hero)
+			})
+	}
+
 	loadHeroes(): void {
 		this.heroService.getHeroes()
 			.subscribe(heroes => this.heroes = heroes)

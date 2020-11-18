c1f487b390bdf678447a0c53c5e681b7479603ac
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-17 22:14:36 -0700

    Add boilerplate for HeroSearch comp

---
+++ refresher/src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-hero-search',
+	templateUrl: './hero-search.component.html',
+	styleUrls: ['./hero-search.component.sass']
+})
+export class HeroSearchComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+}

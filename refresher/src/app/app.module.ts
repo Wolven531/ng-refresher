58dfdd003a152a648899fd5bfc498a02479a0ea2
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-17 22:14:53 -0700

    Expose HeroSearch in AppModule

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component'
 import { CapitalizePipe } from './capitalize.pipe'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'
+import { HeroSearchComponent } from './hero-search/hero-search.component'
 import { HeroesComponent } from './heroes/heroes.component'
 import { InMemoryDataService } from './in-memory-data.service'
 import { MessagesComponent } from './messages/messages.component'
@@ -16,16 +17,17 @@ import { MessagesComponent } from './messages/messages.component'
 	bootstrap: [AppComponent],
 	declarations: [
 		AppComponent,
-		HeroesComponent,
-		CapitalizePipe,
-		HeroDetailComponent,
-		MessagesComponent,
 		DashboardComponent,
+		HeroesComponent,
+		HeroDetailComponent,
+		HeroSearchComponent,
+		MessagesComponent,
+		CapitalizePipe,
 	],
 	imports: [
+		AppRoutingModule,
 		BrowserModule,
 		FormsModule,
-		AppRoutingModule,
 		HttpClientModule,
 		// !!! TODO
 		// The HttpClientInMemoryWebApiModule module intercepts HTTP requests

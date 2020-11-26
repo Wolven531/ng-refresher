6ac39edb4ea78b37c816cf9d9ab834491a064c93
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-25 23:12:02 -0700

    Start debugging why name field isnt populated

--- refresher/src/app/hero/hero-detail/hero-detail.component.ts
+++ refresher/src/app/hero/hero-detail/hero-detail.component.ts
@@ -10,27 +10,41 @@ import { HeroService } from '../hero.service'
 	templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent implements OnInit {
-	@Input() hero: Hero
+	@Input() hero: Hero = {
+		id: -1,
+		name: 'default',
+	}
 
 	constructor(
-		private readonly location: Location,
-		private readonly route: ActivatedRoute,
-		private readonly heroService: HeroService,
-	) { }
+		private location: Location,
+		private route: ActivatedRoute,
+		private heroService: HeroService,
+	) {
+		console.log('[ HeroDetail | ctor ]')
+		// this.hero = {
+		// 	id: -1,
+		// 	name: 'default',
+		// }
+	}
 
 	goBack(): void {
 		this.location.back()
 	}
 
 	ngOnInit(): void {
+		console.log('[ HeroDetail | ngOnInit ]')
 		this.loadHero()
 	}
 
 	loadHero(): void {
+		console.log('[ HeroDetail | loadHero ]')
 		const id = parseInt(this.route.snapshot.paramMap.get('id'), 10)
 
 		this.heroService.getHero(id)
-			.subscribe(hero => this.hero = hero)
+			.subscribe(h => {
+				console.log(`[ HeroDetail | loadHero | callback ] h.name="${h.name}"`)
+				this.hero = h
+			})
 	}
 
 	save(): void {

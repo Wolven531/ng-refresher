0e70277c16261d1ac73448e499cb8ee9d917140e
Anthony Williams <anthony.williams.cs@gmail.com>
2020-11-27 18:37:21 -0700

    Make CamelCase Pipe available to app

--- refresher/src/app/application-pipes/application-pipes.module.ts
+++ refresher/src/app/application-pipes/application-pipes.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { CamelCasePipe } from './camel-case.pipe'
 import { CapitalizePipe } from './capitalize.pipe'
 
 @NgModule({
 	declarations: [
+		CamelCasePipe,
 		CapitalizePipe,
 	],
 	exports: [
+		CamelCasePipe,
 		CapitalizePipe,
 	],
 	imports: [

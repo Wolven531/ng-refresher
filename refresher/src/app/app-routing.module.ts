7c06117138c9fcd9001c8671710e776636c1e5ff
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-01 21:22:32 -0700

    Clean up routing to hero module

--- refresher/src/app/app-routing.module.ts
+++ refresher/src/app/app-routing.module.ts
@@ -1,25 +1,11 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { DashboardComponent } from './dashboard/dashboard.component'
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'
-import { HeroesComponent } from './heroes/heroes.component'
 
-// import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component'
-// import { HeroesComponent } from './hero/heroes/heroes.component'
-
-// TODO - Nov 18, 2020 - extract hero related routes to Hero module
 const routes: Routes = [
 	{
 		component: DashboardComponent,
 		path: 'dashboard',
-	},
-	{
-		component: HeroDetailComponent,
-		path: 'detail/:id',
-	},
-	{
-		component: HeroesComponent,
-		path: 'heroes',
 	},
 	{
 		path: '',

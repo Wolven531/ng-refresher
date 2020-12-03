c42b8b5946bfcd1e74358d94a14f2d4fb9056dca
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-03 00:16:36 -0700

    Add FormsModule - input populates in module temp

--- refresher/src/app/hero/hero.module.ts
+++ refresher/src/app/hero/hero.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { ApplicationPipesModule } from '../application-pipes/application-pipes.module'
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 import { HeroRoutingModule } from './hero-routing.module'
@@ -16,9 +17,10 @@ import { HeroesComponent } from './heroes/heroes.component'
 		HeroSearchComponent, // need to export since app-hero-search is used in some template
 	],
 	imports: [
-		CommonModule,
-		ApplicationPipesModule,
-		HeroRoutingModule,
+		CommonModule, // required in all modules that are not app (which uses BrowserModule)
+		FormsModule, // required so that ngModel is available to components
+		ApplicationPipesModule, // required to provide custom pipes to components
+		HeroRoutingModule, // required to handle routing within module
 	],
 })
 export class HeroModule { }

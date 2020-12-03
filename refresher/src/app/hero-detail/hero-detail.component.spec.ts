ace16d73e93e41f0c3d946e6bbb91f1cd62a0eb2
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-02 23:51:50 -0700

    Comment out old implementation

--- refresher/src/app/hero-detail/hero-detail.component.spec.ts
+++ refresher/src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+// import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { HeroDetailComponent } from './hero-detail.component'
+// import { HeroDetailComponent } from './hero-detail.component'
 
-describe('HeroDetailComponent', () => {
-	let component: HeroDetailComponent
-	let fixture: ComponentFixture<HeroDetailComponent>
+// describe('HeroDetailComponent', () => {
+// 	let component: HeroDetailComponent
+// 	let fixture: ComponentFixture<HeroDetailComponent>
 
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			declarations: [HeroDetailComponent]
-		})
-			.compileComponents()
-	})
+// 	beforeEach(async () => {
+// 		await TestBed.configureTestingModule({
+// 			declarations: [HeroDetailComponent]
+// 		})
+// 			.compileComponents()
+// 	})
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent(HeroDetailComponent)
-		component = fixture.componentInstance
-		fixture.detectChanges()
-	})
+// 	beforeEach(() => {
+// 		fixture = TestBed.createComponent(HeroDetailComponent)
+// 		component = fixture.componentInstance
+// 		fixture.detectChanges()
+// 	})
 
-	it('should create', () => {
-		expect(component).toBeTruthy()
-	})
-})
+// 	it('should create', () => {
+// 		expect(component).toBeTruthy()
+// 	})
+// })

79419270c90fca6377bec8c661ed8b9cc26d89a3
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-03 00:33:51 -0700

    Get HeroDetail tests to pass

--- refresher/src/app/hero/hero-detail/hero-detail.component.spec.ts
+++ refresher/src/app/hero/hero-detail/hero-detail.component.spec.ts
@@ -1,5 +1,11 @@
+import { CommonModule } from '@angular/common'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { FormsModule } from '@angular/forms'
+import { ActivatedRoute } from '@angular/router'
+import { of } from 'rxjs'
+import { ApplicationPipesModule } from '../../application-pipes/application-pipes.module'
+import { HeroService } from '../../hero.service'
+import { Hero } from '../hero.interface'
 import { HeroDetailComponent } from './hero-detail.component'
 
 describe('HeroDetailComponent', () => {
@@ -8,18 +14,45 @@ describe('HeroDetailComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [HeroDetailComponent]
+			declarations: [
+				HeroDetailComponent,
+			],
+			imports: [
+				CommonModule,
+				FormsModule,
+				ApplicationPipesModule,
+			],
+			providers: [
+				{
+					provide: Location,
+					useValue: {
+						back: jasmine.createSpy(),
+					},
+				},
+				{
+					provide: ActivatedRoute,
+					useValue: {
+						params: of({ id: '1' }),
+					},
+				},
+				{
+					provide: HeroService,
+					useValue: {
+						getHero: jasmine.createSpy().and
+							.returnValue(of({ id: 1, name: 'test name' } as Hero)),
+						updateHero: jasmine.createSpy().and
+							.returnValue(of()),
+					},
+				},
+			],
 		})
 			.compileComponents()
-	})
 
-	beforeEach(() => {
 		fixture = TestBed.createComponent(HeroDetailComponent)
 		component = fixture.componentInstance
-		fixture.detectChanges()
 	})
 
-	it('should create', () => {
+	it('creates component', () => {
 		expect(component).toBeTruthy()
 	})
 })

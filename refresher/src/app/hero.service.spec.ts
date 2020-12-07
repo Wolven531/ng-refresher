5adda4421e899e84ba7825bf6973d9b8b00d5e79
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-06 22:22:11 -0700

    Get Hero Svc unit tests passing

--- refresher/src/app/hero.service.spec.ts
+++ refresher/src/app/hero.service.spec.ts
@@ -1,15 +1,37 @@
+import { HttpClient } from '@angular/common/http'
 import { TestBed } from '@angular/core/testing'
+import { of } from 'rxjs'
 import { HeroService } from './hero.service'
+import { MessageService } from './message.service'
 
 describe('HeroService', () => {
 	let service: HeroService
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({})
+		TestBed.configureTestingModule({
+			providers: [
+				{
+					provide: HttpClient,
+					useValue: {
+						delete: jasmine.createSpy().and.returnValue(of({})),
+						get: jasmine.createSpy().and.returnValue(of({})),
+						post: jasmine.createSpy().and.returnValue(of({})),
+						put: jasmine.createSpy().and.returnValue(of({})),
+					},
+				},
+				{
+					provide: MessageService,
+					useValue: {
+						add: jasmine.createSpy(),
+					},
+				},
+			],
+		})
+
 		service = TestBed.inject(HeroService)
 	})
 
-	it('should be created', () => {
+	it('creates service', () => {
 		expect(service).toBeTruthy()
 	})
 })

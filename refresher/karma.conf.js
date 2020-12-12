b9ddd1fb501b5b5921ea509c64aaee6928ba0a35
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-11 22:58:07 -0700

    Use path agnostic fragments in join

--- refresher/karma.conf.js
+++ refresher/karma.conf.js
@@ -1,3 +1,4 @@
+/* global __dirname, require */
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
@@ -11,7 +12,7 @@ module.exports = function (config) {
 		},
 		colors: true,
 		coverageIstanbulReporter: {
-			dir: require('path').join(__dirname, './coverage/refresher'),
+			dir: require('path').join(__dirname, 'coverage', 'refresher'),
 			fixWebpackSourcePaths: true,
 			reports: ['html', 'lcovonly', 'text-summary'],
 			thresholds: {

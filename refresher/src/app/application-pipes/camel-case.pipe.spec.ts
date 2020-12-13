65d4b89859a4b8e3c6ec62d9336350e4d2421d36
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-12 23:28:15 -0700

    Start camel case pipe tests

---
+++ refresher/src/app/application-pipes/camel-case.pipe.spec.ts
@@ -0,0 +1,18 @@
+import { CamelCasePipe } from './camel-case.pipe'
+
+describe('CamelCasePipe', () => {
+	// pipe is a pure, stateless function - no need for beforeEach
+	const pipe = new CamelCasePipe()
+
+	it('handles empty string', () => {
+		expect(pipe.transform('')).toBe('')
+	})
+
+	// it('handles single word (all uppercase)', () => {
+	// 	expect(pipe.transform('ABC')).toBe('aBC')
+	// })
+
+	// it('handles strings w/ spaces (mixed casing)', () => {
+	// 	expect(pipe.transform('hello SWEET wOrLd')).toBe('helloSweetWorld')
+	// })
+})

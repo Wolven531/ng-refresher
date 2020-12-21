d05532ce5131f5042b95086166ce1e5955dcff5a
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-20 23:58:07 -0700

    Update karma config file

--- refresher/karma.conf.js
+++ refresher/karma.conf.js
@@ -4,9 +4,11 @@
 
 module.exports = function (config) {
 	config.set({
-		autoWatch: true,
 		basePath: '',
-		browsers: ['Chrome'],
+		browsers: [
+			// 'Chrome',
+			'ChromeHeadless',
+		],
 		client: {
 			clearContext: false, // leave Jasmine Spec Runner output visible in browser
 		},
@@ -55,16 +57,18 @@ module.exports = function (config) {
 			}
 		},
 		frameworks: ['jasmine', '@angular-devkit/build-angular'],
-		logLevel: config.LOG_INFO,
+		logLevel: config.LOG_INFO, // default val
 		plugins: [
-			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
+			require('karma-coverage'),
+			require('karma-jasmine'),
 			require('karma-jasmine-html-reporter'),
 			require('@angular-devkit/build-angular/plugins/karma'),
 		],
-		port: 9876,
-		// reporters: ['progress', 'html', 'text-summary'],
-		restartOnFileChange: true,
-		singleRun: false,
+		port: 9876, // default val
+		restartOnFileChange:true,
+		retryLimit: 2, // default val
+		reporters: ['progress', 'coverage'],
+		singleRun: true,
 	})
 }

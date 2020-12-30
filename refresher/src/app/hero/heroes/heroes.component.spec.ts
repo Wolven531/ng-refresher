75f95536302ffa693145634477a6195955a2af07
Anthony Williams <anthony.williams.cs@gmail.com>
2020-12-29 23:14:59 -0700

    Add unit test for deleteHero

--- refresher/src/app/hero/heroes/heroes.component.spec.ts
+++ refresher/src/app/hero/heroes/heroes.component.spec.ts
@@ -8,16 +8,18 @@ import { Hero } from '../hero.interface'
 import { HeroesComponent } from './heroes.component'
 
 describe('HeroesComponent', () => {
+	const MOCK_HERO_1: Hero = { id: 1, name: 'test name' }
+	const MOCK_HERO_2: Hero = { id: 2, name: 'test name 2' }
+
 	let component: HeroesComponent
 	let fixture: ComponentFixture<HeroesComponent>
+	let mockDeleteHero: jasmine.Spy
 	let mockGetHeroes: jasmine.Spy
 
 	beforeEach(async () => {
+		mockDeleteHero = jasmine.createSpy().and.returnValue(of())
 		mockGetHeroes = jasmine.createSpy().and.returnValue(
-			of([
-				{ id: 1, name: 'test name' } as Hero,
-				{ id: 2, name: 'test name 2' } as Hero,
-			])
+			of([ MOCK_HERO_1, MOCK_HERO_2 ])
 		)
 
 		await TestBed.configureTestingModule({
@@ -33,6 +35,7 @@ describe('HeroesComponent', () => {
 				{
 					provide: HeroService,
 					useValue: {
+						deleteHero: mockDeleteHero,
 						getHeroes: mockGetHeroes,
 					},
 				},
@@ -60,6 +63,18 @@ describe('HeroesComponent', () => {
 		it('invokes HeroService.getHeroes()', () => {
 			expect(spyLoadHeroes).toHaveBeenCalledTimes(1)
 			expect(mockGetHeroes).toHaveBeenCalledTimes(1)
+		})
+
+		describe('invoke deleteHero() w/ MOCK_HERO_1', () => {
+			beforeEach(() => {
+				component.deleteHero(MOCK_HERO_1)
+			})
+
+			it('invokes HeroService.deleteHero(), removes proper hero from heroes', () => {
+				expect(mockDeleteHero).toHaveBeenCalledTimes(1)
+				expect(mockDeleteHero).toHaveBeenCalledWith(MOCK_HERO_1)
+				expect(component.heroes).toEqual([MOCK_HERO_2])
+			})
 		})
 	})
 })

b8da0e27baf5c48dbb47ba232481320639662404
Anthony Williams <anthony.williams.cs@gmail.com>
2021-01-08 20:37:23 -0700

    Add unit test for getHeroes()

--- refresher/src/app/hero.service.spec.ts
+++ refresher/src/app/hero.service.spec.ts
@@ -1,24 +1,20 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { TestBed } from '@angular/core/testing'
 import { of } from 'rxjs'
 import { HeroService } from './hero.service'
 import { MessageService } from './message.service'
 
 describe('HeroService', () => {
+	let mockNet: HttpClient
 	let service: HeroService
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
+			imports: [
+				// TODO - replace w/ Http test module
+				HttpClientModule,
+			],
 			providers: [
-				{
-					provide: HttpClient,
-					useValue: {
-						delete: jasmine.createSpy().and.returnValue(of({})),
-						get: jasmine.createSpy().and.returnValue(of({})),
-						post: jasmine.createSpy().and.returnValue(of({})),
-						put: jasmine.createSpy().and.returnValue(of({})),
-					},
-				},
 				{
 					provide: MessageService,
 					useValue: {
@@ -29,9 +25,26 @@ describe('HeroService', () => {
 		})
 
 		service = TestBed.inject(HeroService)
+		mockNet = TestBed.inject(HttpClient)
 	})
 
 	it('creates service', () => {
 		expect(service).toBeTruthy()
+	})
+
+	describe('invoke getHeroes()', () => {
+		let spyGet: jasmine.Spy
+
+		beforeEach(() => {
+			spyGet = spyOn(mockNet, 'get').and.returnValue(of([]))
+
+			service.getHeroes()
+		})
+
+		it('invokes HttpClient.get() properly', () => {
+			expect(spyGet).toHaveBeenCalledTimes(1)
+			// private member property, use string accessor to avoid cast to any
+			expect(spyGet).toHaveBeenCalledWith(HeroService['ENDPOINT_HEROES'])
+		})
 	})
 })

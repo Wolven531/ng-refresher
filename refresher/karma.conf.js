d3662967f1dbb3346ae0b60d78ccfebd56c524bf
Anthony Williams <anthony.williams.cs@gmail.com>
2021-01-11 21:21:39 -0700

    Update karma doc link

--- refresher/karma.conf.js
+++ refresher/karma.conf.js
@@ -1,6 +1,6 @@
 /* global __dirname, require */
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/5.1/config/configuration-file.html
 
 module.exports = function (config) {
 	config.set({

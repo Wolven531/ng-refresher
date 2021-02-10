d83fd5ef6cfd368e8aad2d77fe44e036e179da58
wolven531 <anthony.williams.cs@gmail.com>
2021-02-09 20:51:56 -0700

    Refactor App unit tests
    * Fix warnings
    * Update test wording
    * Use standard test structure

--- refresher/src/app/app.component.spec.ts
+++ refresher/src/app/app.component.spec.ts
@@ -1,24 +1,32 @@
-import { TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component'
+import { MessagesComponent } from './messages/messages.component'
 
 describe('AppComponent', () => {
+	let component: AppComponent
+	let fixture: ComponentFixture<AppComponent>
+
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [
-				AppComponent
+				AppComponent,
+				MessagesComponent,
+			],
+			imports: [
+				RouterModule.forRoot([]),
 			],
 		}).compileComponents()
+
+		fixture = TestBed.createComponent(AppComponent)
+		component = fixture.componentInstance
 	})
 
-	it('should create the app', () => {
-		const fixture = TestBed.createComponent(AppComponent)
-		const app = fixture.componentInstance
-		expect(app).toBeTruthy()
+	it('creates component', () => {
+		expect(component).toBeTruthy()
 	})
 
-	it(`should have as title 'refresher'`, () => {
-		const fixture = TestBed.createComponent(AppComponent)
-		const app = fixture.componentInstance
-		expect(app.title).toEqual('refresher')
+	it(`has title 'refresher'`, () => {
+		expect(component.title).toEqual('refresher')
 	})
 })

3b32d99ad9f41da272e83cb0c2a9f6f5b019987c
wolven531 <anthony.williams.cs@gmail.com>
2021-02-22 21:31:06 -0700

    Add geolocation code

--- refresher/src/app/game/game.component.ts
+++ refresher/src/app/game/game.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core'
 
+interface GeoCoord {
+	accuracy: number
+	altitude: number
+	altitudeAccuracy: number
+	heading: number
+	latitude: number
+	longitude: number
+	speed: number
+}
+
+interface GeoPos {
+	coords: GeoCoord
+	timestamp: number
+}
+
 @Component({
 	selector: 'app-game',
 	templateUrl: './game.component.html',
 	styleUrls: ['./game.component.sass']
 })
 export class GameComponent implements OnInit {
-	constructor() { }
-
 	ngOnInit(): void {
+		if (!window.navigator.geolocation) {
+			return
+		}
+
+		window.navigator.geolocation.getCurrentPosition(
+			this.handlePositionLoaded,
+			this.handlePositionError,
+			{
+				enableHighAccuracy: true,
+			}
+		)
+	}
+
+	private handlePositionLoaded(position: GeoPos): void {
+		position.coords.accuracy
+		position.coords.altitude
+		position.coords.altitudeAccuracy
+		position.coords.heading
+		position.coords.latitude
+		position.coords.longitude
+		position.coords.speed
+	}
+
+	private handlePositionError(): void {
 	}
 }

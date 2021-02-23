e92791e46c26a6df12afdf7a3f9255ba4504647f
wolven531 <anthony.williams.cs@gmail.com>
2021-02-22 21:33:28 -0700

    Add 'game' to routes

--- refresher/src/app/app-routing.module.ts
+++ refresher/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { GameComponent } from './game/game.component'
 
 const routes: Routes = [
 	{
 		component: DashboardComponent,
 		path: 'dashboard',
+	},
+	{
+		component: GameComponent,
+		path: 'game',
 	},
 	{
 		path: '',

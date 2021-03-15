88e9ea5e7e8d9c2054e003e4a57950557d2c4ac6
wolven531 <anthony.williams.cs@gmail.com>
2021-03-14 23:44:10 -0600

    Import ReactiveForms

--- refresher/src/app/app.module.ts
+++ refresher/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
 import { AppRoutingModule } from './app-routing.module'
@@ -24,6 +24,7 @@ import { GameComponent } from './game/game.component'
 	imports: [
 		BrowserModule,
 		FormsModule,
+		ReactiveFormsModule,
 		HttpClientModule,
 		// !!! TODO
 		// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
